e9fdd2bdb590458a38d0f2fcc7f4a1c356f937fe
panagioths-k <panos_kotsikoris@hotmail.com>
2020-05-17 18:25:39 +0300

    add marker colors

--- src/main/java/com/spring/smarttrashmanagementsystem/Model/MarkerRating.java
+++ src/main/java/com/spring/smarttrashmanagementsystem/Model/MarkerRating.java
@@ -2,5 +2,13 @@ package com.spring.smarttrashmanagementsystem.Model;
 
 public enum MarkerRating {
 
-    GREEN, YELLOW, RED
+    HUE_GREEN(120.0F),
+    HUE_YELLOW(60.0F),
+    HUE_RED(0.0F);
+
+    public final Float value;
+
+    private MarkerRating(Float value) {
+        this.value = value;
+    }
 }
--- src/main/java/com/spring/smarttrashmanagementsystem/controller/MarkerController.java
+++ src/main/java/com/spring/smarttrashmanagementsystem/controller/MarkerController.java
@@ -17,12 +17,14 @@ public class MarkerController {
     @RequestMapping(method = RequestMethod.GET, value = "/markers", produces = "application/json")
     public ResponseEntity<List<MarkerDto>> getMarkers() {
 
-        MarkerDto m1 = new MarkerDto("40.631412", "22.943392", MarkerRating.GREEN);
-        MarkerDto m2 = new MarkerDto("40.635939", "22.940673", MarkerRating.RED);
+        MarkerDto m1 = new MarkerDto("40.631412", "22.943392", MarkerRating.HUE_GREEN);
+        MarkerDto m2 = new MarkerDto("40.635939", "22.940673", MarkerRating.HUE_RED);
+        MarkerDto m3 = new MarkerDto("40.632088", "22.951764", MarkerRating.HUE_YELLOW);
 
         List<MarkerDto> markers = new ArrayList<>();
         markers.add(m1);
         markers.add(m2);
+        markers.add(m3);
         return new ResponseEntity<List<MarkerDto>>(markers, HttpStatus.OK);
     }
 }
